2d4ee0d7280d97a9a914189352eb728a6b4691ff
Skylar Grammas <skylarg@skylarg-macbookpro.roam.corp.google.com>
2018-11-19 18:20:42 -0800

    Empty game board after a player wins

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -127,13 +127,18 @@ export class AppComponent {
   endTurn() {
     this.gameEnded = this.game.currentPlayer().pieceLocation.typeOfState === 'End';
     if (this.gameEnded) {
+      this.transitionCards = [];
+      this.stepCard = null;
+      this.game.players = [];
+      this.game = null;
       return;
+    } else {
+      this.game.nextPlayer();
+      this.currentPlayer = this.game.currentPlayer().name;
+      this.stepCard = null;
+      this.transitionCards = [];
+      this.transitionCardsActive = false;
     }
-    this.game.nextPlayer();
-    this.currentPlayer = this.game.currentPlayer().name;
-    this.stepCard = null;
-    this.transitionCards = [];
-    this.transitionCardsActive = false;
   }
 
   renderBoard() {
